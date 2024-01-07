15a5aad8be2e0901e4cfb3ee42f3908193fba05f
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-01-06 21:12:58 -0600

    moment

--- script.sh
+++ script.sh
@@ -150,6 +150,17 @@ _download_boot_files() {
     fi
     
     rm -rf BuildManifest.plist
+    
+    ./ipatcher $1/$3/iBSS.dec $1/$3/iBSS.patched
+    ./ipatcher $1/$3/iBEC.dec $1/$3/iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
+    ./img4 -i $1/$3/iBSS.patched -o $1/$3/iBSS.img4 -M IM4M -A -T ibss
+    ./img4 -i $1/$3/iBEC.patched -o $1/$3/iBEC.img4 -M IM4M -A -T ibec
+    # need to generate kcache.raw somehow
+    ./seprmvr64lite $1/$3/kcache.raw $1/$3/kcache.patched
+    ./kerneldiff $1/$3/kcache.raw $1/$3/kcache.patched $1/$3/kc.bpatch
+    ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
+    ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache -M IM4M -T krnl -P $1/$3/kc.bpatch
+    ./img4 -i $1/$3/DeviceTree.dec -o $1/$3/devicetree.img4 -A -M IM4M -T rdtr
 }
 
 _download_root_fs() {
@@ -282,93 +293,6 @@ if [ ! -e ramdisk/ramdisk.img4 ]; then
     ./img4 -i ramdisk/kernelcache.dec -o ramdisk/kernelcache.img4 -M IM4M -T rkrn
     ./img4 -i ramdisk/devicetree.dec -o ramdisk/devicetree.img4 -A -M IM4M -T rdtr
 fi
-mkdir work
-cp IM4M work
-cd work
-if [ ! -e devicetree.img4 ]; then
-    echo $iosversion
-    if [ "$iosversion" = '8.4.1' ]; then
-        ../pzb -g Firmware/dfu/iBSS.n51.RELEASE.im4p "$ipswurl2"
-        ../pzb -g Firmware/dfu/iBEC.n51.RELEASE.im4p "$ipswurl2"
-        ../pzb -g kernelcache.release.n51 "$ipswurl2"
-        ../pzb -g Firmware/all_flash/all_flash.n51ap.production/DeviceTree.n51ap.im4p "$ipswurl2"
-        ../img4 -i iBSS.n51.RELEASE.im4p -o iBSS.dec -k 46c3abc7147db7e9c06aae801b13a91238b9f71efaaa02e48731471ac1fc506ab1e4e9716eac2207037778d9f62648d9
-        ../img4 -i iBEC.n51.RELEASE.im4p -o iBEC.dec -k c52d431c7fbc85b67307c2c7297f919f5fd45b3e2717b75e9ef1816f6afa2aa9e92fb8c7f1b1403600943a8bd637b62d
-        #../ipatcher iBSS.dec iBSS.patched
-        #../ipatcher iBEC.dec iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
-        ../iBoot64Patcher iBSS.dec iBSS.patched
-        ../iBoot64Patcher iBEC.dec iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
-        ../img4 -i iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
-        ../img4 -i iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
-        ../img4 -i kernelcache.release.n51 -o kernelcache.im4p -k 03447866614ec7f0e083eba37b31f1a75484c5ab65e00e895b95db81b873d1292f766e614c754ec523b62a48d33664e1 -D
-        ../img4 -i kernelcache.release.n51 -o kcache.raw -k 03447866614ec7f0e083eba37b31f1a75484c5ab65e00e895b95db81b873d1292f766e614c754ec523b62a48d33664e1
-        ../seprmvr64lite kcache.raw kcache.patched
-        ../kerneldiff kcache.raw kcache.patched kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache.img4 -M IM4M -T rkrn -P kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache -M IM4M -T krnl -P kc.bpatch
-        ../img4 -i DeviceTree.n51ap.im4p -o dtree.raw -k 2f744c5a6cda23c30eccb2fcac9aff2222ad2b37ed96f14a3988102558e0920905536622b1e78288c2533a7de5d01425
-        ../dtree_patcher dtree.raw dree.patched -n
-        ../img4 -i dtree.patched -o devicetree.img4 -A -M IM4M -T rdtr
-    elif [ "$iosversion" = '7.1.2' ]; then
-        ../pzb -g Firmware/dfu/iBSS.n51ap.RELEASE.im4p "$ipswurl1"
-        ../pzb -g Firmware/dfu/iBEC.n51ap.RELEASE.im4p "$ipswurl1"
-        ../pzb -g kernelcache.release.n51 "$ipswurl1"
-        ../pzb -g Firmware/all_flash/all_flash.n51ap.production/DeviceTree.n51ap.im4p "$ipswurl1"
-        ../img4 -i iBSS.n51ap.RELEASE.im4p -o iBSS.dec -k b4c6843dddc7c7e3727077aee6e62c4c42d112d57eeb50505c1e7be26f4d580982839da4e75cc0eb1314ecc464ec4779
-        ../img4 -i iBEC.n51ap.RELEASE.im4p -o iBEC.dec -k f800c184406ae951847b5e0207f78c89058cd17ac2e346dd315a3bdbe8b43565962896ed8bd28bfbd201c94ba94b6afb
-        ../ipatcher iBSS.dec iBSS.patched
-        ../ipatcher iBEC.dec iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
-        ../img4 -i iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
-        ../img4 -i iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
-        ../img4 -i kernelcache.release.n51 -o kernelcache.im4p -k 315af5407859bf02143283deaa31e92e46c6ca6cb9799a6018c94d2cebb6579345f8496d5a0e72cf783e4bd4dbabc59c -D
-        ../img4 -i kernelcache.release.n51 -o kcache.raw -k 315af5407859bf02143283deaa31e92e46c6ca6cb9799a6018c94d2cebb6579345f8496d5a0e72cf783e4bd4dbabc59c
-        ../seprmvr64lite kcache.raw kcache.patched
-        ../kerneldiff kcache.raw kcache.patched kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache.img4 -M IM4M -T rkrn -P kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache -M IM4M -T krnl -P kc.bpatch
-        ../img4 -i DeviceTree.n51ap.im4p -o dtree.raw -k 4955c27b46e5eaf7e7b3829da15fefcf83f82bb816d59b13451b1fbc21332b730f5138e40148f8815767f1f92b0f16cb
-        ../img4 -i dtree.raw -o devicetree.img4 -A -M IM4M -T rdtr
-    elif [ "$iosversion" = '7.0.6' ]; then
-        ../pzb -g Firmware/dfu/iBSS.n51ap.RELEASE.im4p "$ipswurl4"
-        ../pzb -g Firmware/dfu/iBEC.n51ap.RELEASE.im4p "$ipswurl4"
-        ../pzb -g kernelcache.release.n51 "$ipswurl4"
-        ../pzb -g Firmware/all_flash/all_flash.n51ap.production/DeviceTree.n51ap.im4p "$ipswurl4"
-        ../img4 -i iBSS.n51ap.RELEASE.im4p -o iBSS.dec -k 5841c33639bcf387bd2b72e111d4de6bb3b9eefee0d3f5f3a2f6a0ead7ebc4624fdd532a8e234dda60a0f6b0648bd28e
-        ../img4 -i iBEC.n51ap.RELEASE.im4p -o iBEC.dec -k a49c5193c3f67574f83d477f5a330b4e9110c4fce69d97abee13dd632e3bb485b9d0e30c51b63f3dd56871d6e7793ef7
-        ../ipatcher iBSS.dec iBSS.patched
-        ../ipatcher iBEC.dec iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
-        ../img4 -i iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
-        ../img4 -i iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
-        ../img4 -i kernelcache.release.n51 -o kernelcache.im4p -k 64059127bd207cc0e7a86c1fd7395554cdc5fb1fd3e4da5022b5c4865d6b0fa93d3db59da1f9c1de0f1e77d736f2e0a6 -D
-        ../img4 -i kernelcache.release.n51 -o kcache.raw -k 64059127bd207cc0e7a86c1fd7395554cdc5fb1fd3e4da5022b5c4865d6b0fa93d3db59da1f9c1de0f1e77d736f2e0a6
-        ../seprmvr64lite kcache.raw kcache.patched
-        ../kerneldiff kcache.raw kcache.patched kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache.img4 -M IM4M -T rkrn -P kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache -M IM4M -T krnl -P kc.bpatch
-        ../img4 -i DeviceTree.n51ap.im4p -o dtree.raw -k d14f7f0634d9e1e8e6b581729e056574354a1c1eed8444a2e5f9ab47e5d2308e52f938f456d54a59fad695d3904138a8
-        ../img4 -i dtree.raw -o devicetree.img4 -A -M IM4M -T rdtr
-    elif [ "$iosversion" = '9.3.2' ]; then
-        ../pzb -g Firmware/dfu/iBSS.n51.RELEASE.im4p "$ipswurl3"
-        ../pzb -g Firmware/dfu/iBEC.n51.RELEASE.im4p "$ipswurl3"
-        ../pzb -g kernelcache.release.n51 "$ipswurl3"
-        ../pzb -g Firmware/all_flash/all_flash.n51ap.production/DeviceTree.n51ap.im4p "$ipswurl3"
-        ../img4 -i iBSS.n51.RELEASE.im4p -o iBSS.dec -k fcd5ce2c70f483d50add94d63cc718724618dc046b4c6e432c81243e6f94cdff2f9b0b899a050f0870bb913860f97951
-        ../img4 -i iBEC.n51.RELEASE.im4p -o iBEC.dec -k e73bf307e7f8783ead2a6cbed9a2aea3ebf3b332e5dda4e94f88fe0899e30731dcbb1e5e03a5b6757a4c32cc298a018f
-        ../iBoot64Patcher iBSS.dec iBSS.patched
-        ../iBoot64Patcher iBEC.dec iBEC.patched -b "-v rd=disk0s1s1 amfi=0xff cs_enforcement_disable=1 keepsyms=1 debug=0x2014e wdt=-1"
-        ../img4 -i iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
-        ../img4 -i iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
-        ../img4 -i kernelcache.release.n51 -o kernelcache.im4p -k 1794b612cf3a4781cebd976c55f5c23abef2d346023dee0e7154673d4adfdee7d6ca1854e7107648a1e3f004f9add1be -D
-        ../img4 -i kernelcache.release.n51 -o kcache.raw -k 1794b612cf3a4781cebd976c55f5c23abef2d346023dee0e7154673d4adfdee7d6ca1854e7107648a1e3f004f9add1be
-        ../seprmvr64lite kcache.raw kcache.patched
-        ../kerneldiff kcache.raw kcache.patched kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache.img4 -M IM4M -T rkrn -P kc.bpatch
-        ../img4 -i kernelcache.im4p -o kernelcache -M IM4M -T krnl -P kc.bpatch
-        ../img4 -i DeviceTree.n51ap.im4p -o dtree.raw -k dc34b39adb91850be325246269da6c12eaed50c730a90bb566c638644fef398412a1b552d97aa8e764df0a30a700d05b
-        ../img4 -i dtree.raw -o devicetree.img4 -A -M IM4M -T rdtr
-    fi
-fi
-cd ..
 _wait_for_dfu
 ./ipwnder -p
 ./irecovery -f iBSS.img4
