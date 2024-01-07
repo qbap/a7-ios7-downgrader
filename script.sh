d1dd816a62f298642cb3b0b726bae81a09c65dce
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-01-06 21:01:05 -0600

    meoww

--- script.sh
+++ script.sh
@@ -19,6 +19,72 @@ remote_cmd() {
 remote_cp() {
     ./sshpass -p 'alpine' scp -o StrictHostKeyChecking=no -P2222 $@
 }
+_download_ramdisk_boot_files() {
+    # $deviceid arg 1
+    # $replace arg 2
+    # $version arg 3
+
+    ipswurl=$(curl -k -sL "https://api.ipsw.me/v4/device/$deviceid?type=ipsw" | ./jq '.firmwares | .[] | select(.version=="'$3'")' | ./jq -s '.[0] | .url' --raw-output)
+
+    # Copy required library for taco to work to /usr/bin
+    sudo rm -rf /usr/bin/img4
+    sudo cp ./img4 /usr/bin/img4
+    sudo rm -rf /usr/local/bin/img4
+    sudo cp ./img4 /usr/local/bin/img4
+
+    # Copy required library for taco to work to /usr/bin
+    sudo rm -rf /usr/bin/dmg
+    sudo cp ./dmg /usr/bin/dmg
+    sudo rm -rf /usr/local/bin/dmg
+    sudo cp ./dmg /usr/local/bin/dmg
+
+    rm -rf BuildManifest.plist
+
+    mkdir -p ramdisk
+
+    ./pzb -g BuildManifest.plist "$ipswurl"
+
+    if [ ! -e ramdisk/kernelcache.dec ]; then
+        # Download kernelcache
+        ./pzb -g $(awk "/""$2""/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1) "$ipswurl"
+        # Decrypt kernelcache
+        # note that as per src/decrypt.rs it will not rename the file
+        cargo run decrypt $1 $3 $(awk "/""$2""/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1) -l
+        # so we shall rename the file ourselves
+        mv $(awk "/""$2""/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1) ramdisk/kernelcache.dec
+    
+    fi
+
+    if [ ! -e ramdisk/iBSS.dec ]; then
+        # Download iBSS
+        ./pzb -g $(awk "/""$2""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+        # Decrypt iBSS
+        ./gaster decrypt $(awk "/""$2""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//') ramdisk/iBSS.dec
+    fi
+
+    if [ ! -e ramdisk/iBEC.dec ]; then
+        # Download iBEC
+        ./pzb -g $(awk "/""$2""/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+        # Decrypt iBEC
+        ./gaster decrypt $(awk "/""$2""/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//') ramdisk/iBEC.dec
+    fi
+
+    if [ ! -e ramdisk/DeviceTree.dec ]; then
+        # Download DeviceTree
+        ./pzb -g $(awk "/""$2""/{x=1}x&&/DeviceTree[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+        # Decrypt DeviceTree
+        ./gaster decrypt $(awk "/""$2""/{x=1}x&&/DeviceTree[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]all_flash.*production[/]//') ramdisk/DeviceTree.dec
+    fi
+
+    if [ ! -e ramdisk/RestoreRamDisk.dec ]; then
+        # Download RestoreRamDisk
+        ./pzb -g "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" "$ipswurl"
+        # Decrypt RestoreRamDisk
+        ./gaster decrypt "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" ramdisk/RestoreRamDisk.dec
+    fi
+    
+    rm -rf BuildManifest.plist
+}
 _download_boot_files() {
     # $deviceid arg 1
     # $replace arg 2
@@ -76,6 +142,41 @@ _download_boot_files() {
         ./gaster decrypt $(awk "/""$2""/{x=1}x&&/DeviceTree[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]all_flash.*production[/]//') $1/$3/DeviceTree.dec
     fi
 
+    if [ ! -e $1/$3/RestoreRamDisk.dec ]; then
+        # Download RestoreRamDisk
+        ./pzb -g "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" "$ipswurl"
+        # Decrypt RestoreRamDisk
+        ./gaster decrypt "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" $1/$3/RestoreRamDisk.dec
+    fi
+    
+    rm -rf BuildManifest.plist
+}
+
+_download_root_fs() {
+    # $deviceid arg 1
+    # $replace arg 2
+    # $version arg 3
+
+    ipswurl=$(curl -k -sL "https://api.ipsw.me/v4/device/$deviceid?type=ipsw" | ./jq '.firmwares | .[] | select(.version=="'$3'")' | ./jq -s '.[0] | .url' --raw-output)
+
+    # Copy required library for taco to work to /usr/bin
+    sudo rm -rf /usr/bin/img4
+    sudo cp ./img4 /usr/bin/img4
+    sudo rm -rf /usr/local/bin/img4
+    sudo cp ./img4 /usr/local/bin/img4
+
+    # Copy required library for taco to work to /usr/bin
+    sudo rm -rf /usr/bin/dmg
+    sudo cp ./dmg /usr/bin/dmg
+    sudo rm -rf /usr/local/bin/dmg
+    sudo cp ./dmg /usr/local/bin/dmg
+
+    rm -rf BuildManifest.plist
+
+    mkdir -p $1/$3
+
+    ./pzb -g BuildManifest.plist "$ipswurl"
+
     if [ ! -e $1/$3/OS.dec ]; then
         # Download root fs
         ./pzb -g "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."OS"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" "$ipswurl"
@@ -85,14 +186,16 @@ _download_boot_files() {
         osfn="$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."OS"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)"
         mv $(echo $osfn | sed "s/dmg/bin/g") $1/$3/OS.dec
     fi
-
-    if [ ! -e $1/$3/RestoreRamDisk.dec ]; then
-        # Download RestoreRamDisk
-        ./pzb -g "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" "$ipswurl"
-        # Decrypt RestoreRamDisk
-        ./gaster decrypt "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" $1/$3/RestoreRamDisk.dec
-    fi
     
+    if [ ! -e $1/$3/OS.tar ]; then
+        ./dmg build $1/$3/OS.dec $1/$3/OS.dmg
+        hdiutil attach -mountpoint /tmp/ios $1/$3/OS.dmg
+        sudo diskutil enableOwnership /tmp/ios
+        sudo ./gnutar -cvf $1/$3/OS.tar -C /tmp/ios .
+        hdiutil detach /tmp/ios
+        rm -rf /tmp/ios
+    fis
+
     rm -rf BuildManifest.plist
 }
 _kill_if_running() {
@@ -140,11 +243,7 @@ _wait_for_dfu
 check=$(./irecovery -q | grep CPID | sed 's/CPID: //')
 replace=$(./irecovery -q | grep MODEL | sed 's/MODEL: //')
 deviceid=$(./irecovery -q | grep PRODUCT | sed 's/PRODUCT: //')
-ipswurl=$(curl -k -sL "https://api.ipsw.me/v4/device/$deviceid?type=ipsw" | ./jq '.firmwares | .[] | select(.version=="'$1'")' | ./jq -s '.[0] | .url' --raw-output)
-ipswurl2=$(curl -k -sL "https://api.ipsw.me/v4/device/$deviceid?type=ipsw" | ./jq '.firmwares | .[] | select(.version=="'8.4.1'")' | ./jq -s '.[0] | .url' --raw-output)
 echo $deviceid
-echo $ipswurl
-echo $ipswurl2
 # if we already have installed ios using this script we can just boot the existing kernelcache
 if [ -e work/iBSS.img4 ]; then
     _wait_for_dfu
@@ -162,94 +261,26 @@ fi
 # we need a shsh file that we can use in order to boot the ios 8 ramdisk
 # in this case we are going to use the ones from SSHRD_Script https://github.com/verygenericname/SSHRD_Script
 ./img4tool -e -s other/shsh/"${check}".shsh -m IM4M
-# as of the time im writing this, i have only tested this script on iPhone6,1 not iPhone6,2
-# pls let me know if it works on iPhone6,2 as is, and if it doesnt ill fix it
-if [[ "$deviceid" = 'iPhone6,1' || "$deviceid" = 'iPhone6,2' ]]; then
-    echo "ok"
-else
-    echo "this device is not supported"
-fi
-if [ "$iosversion" = '8.4.1' ]; then
-    # we need to download ios 8.4.1 root fs and decrypt it and put it into a tar file
-    if [ ! -e 058-24099-024.dmg ]; then
-        ./pzb -g 058-24099-024.dmg "$ipswurl2"
-        rm -rf rw.dmg
-        ./dmg extract 058-24099-024.dmg rw.dmg -k e8427cfcf8ff5c79b43453784eec6fead8ca780a7500fe8c17187c9919c9b51800512daf
-        ./dmg build rw.dmg ios8.dmg
-        hdiutil attach -mountpoint /tmp/ios8 ios8.dmg
-        sudo diskutil enableOwnership /tmp/ios8
-        sudo ./gnutar -cvf ios8.tar -C /tmp/ios8 .
-    fi
-elif [ "$iosversion" = '7.0.6' ]; then
-    # we need to download ios 7.0.6 root fs and decrypt it and put it into a tar file
-    if [ ! -e 058-2384-003.dmg ]; then
-        ./pzb -g 058-2384-003.dmg "$ipswurl4"
-        rm -rf rw.dmg
-        ./dmg extract 058-2384-003.dmg rw.dmg -k a0d2c1d5ce091c62dcf28f1ac953e58a6a66c112184d125e177a1285b7e991262e3c5917
-        ./dmg build rw.dmg ios706.dmg
-        hdiutil attach -mountpoint /tmp/ios7 ios706.dmg
-        sudo diskutil enableOwnership /tmp/ios7
-        sudo ./gnutar -cvf ios706.tar -C /tmp/ios7 .
-    fi
-elif [ "$iosversion" = '7.1.2' ]; then
-    # we need to download ios 7.1.2 root fs and decrypt it and put it into a tar file
-    if [ ! -e 058-4438-009.dmg ]; then
-        ./pzb -g 058-4438-009.dmg "$ipswurl1"
-        rm -rf rw.dmg
-        ./dmg extract 058-4438-009.dmg rw.dmg -k ff95d392a307dfd6bb4f6d9ad4ef5db2ab50e015cee5366b5195090a1b4f4c84a86f30f5
-        ./dmg build rw.dmg ios7.dmg
-        hdiutil attach -mountpoint /tmp/ios7 ios7.dmg
-        sudo diskutil enableOwnership /tmp/ios7
-        sudo ./gnutar -cvf ios712.tar -C /tmp/ios7 .
-    fi
-elif [ "$iosversion" = '9.3.2' ]; then
-    # we need to download ios 9.3.2 root fs and decrypt it and put it into a tar file
-    if [ ! -e 058-36981-072.dmg ]; then
-        ./pzb -g 058-36981-072.dmg "$ipswurl3"
-        rm -rf rw.dmg
-        ./dmg extract 058-36981-072.dmg rw.dmg -k ca08477e9fdba12761bf5fbe2466f3028df6debe633b87cb2d66911c2826646fc3cbfc3c
-        ./dmg build rw.dmg ios9.dmg
-        hdiutil attach -mountpoint /tmp/ios9 ios9.dmg
-        sudo diskutil enableOwnership /tmp/ios9
-        sudo ./gnutar -cvf ios9.tar -C /tmp/ios9 .
-    fi
-else
-    echo "that version is not supported"
-fi
-# we need to download restore ramdisk for ios 8.4.1
+_download_ramdisk_boot_files $deviceid $replace 9.3.2
+_download_boot_files $deviceid $replace $1
+_download_root_fs $deviceid $replace $1
+# we need to download restore ramdisk for ios 9.3.2
 # in this example we are using a modified copy of the ssh tar from SSHRD_Script https://github.com/verygenericname/SSHRD_Script
 # this modified copy of the ssh tar fixes a few issues on ios 8 and adds some executables we need
-if [ ! -e 058-24442-023.dmg ]; then
-    ./pzb -g 058-24442-023.dmg "$ipswurl2"
-    ./img4 -i 058-24442-023.dmg -o ramdisk.dmg -k 5f72aa47ded95dd5f3504c44db082240a8faf901c15014e99c6bf50a63c407c82846f07750b12fa5737ede556b226619
-    hdiutil resize -size 60M ramdisk.dmg
-    hdiutil attach -mountpoint /tmp/ramdisk ramdisk.dmg
+if [ ! -e ramdisk/ramdisk.img4 ]; then
+    hdiutil resize -size 60M ramdisk/RestoreRamDisk.dec
+    hdiutil attach -mountpoint /tmp/ramdisk ramdisk/RestoreRamDisk.dec
     sudo diskutil enableOwnership /tmp/ramdisk
     sudo ./gnutar -xvf iram.tar -C /tmp/ramdisk
-    #sudo ./gnutar -xvf dualbootstuff.tar -C /tmp/ramdisk
     hdiutil detach /tmp/ramdisk
-    ./img4tool -c ramdisk.im4p -t rdsk ramdisk.dmg
-    ./img4tool -c ramdisk.img4 -p ramdisk.im4p -m IM4M
-fi
-# if the ramdisk does not exist, exit
-if [ ! -e ramdisk.img4 ]; then
-    exit
-fi
-if [ ! -e devicetree.img4 ]; then
-    ./pzb -g Firmware/dfu/iBSS.n51.RELEASE.im4p "$ipswurl2"
-    ./pzb -g Firmware/dfu/iBEC.n51.RELEASE.im4p "$ipswurl2"
-    ./pzb -g kernelcache.release.n51 "$ipswurl2"
-    ./pzb -g Firmware/all_flash/all_flash.n51ap.production/DeviceTree.n51ap.im4p "$ipswurl2"
-    ./img4 -i iBSS.n51.RELEASE.im4p -o iBSS.dec -k 46c3abc7147db7e9c06aae801b13a91238b9f71efaaa02e48731471ac1fc506ab1e4e9716eac2207037778d9f62648d9
-    ./img4 -i iBEC.n51.RELEASE.im4p -o iBEC.dec -k c52d431c7fbc85b67307c2c7297f919f5fd45b3e2717b75e9ef1816f6afa2aa9e92fb8c7f1b1403600943a8bd637b62d
-    ./ipatcher iBSS.dec iBSS.patched
-    ./ipatcher iBEC.dec iBEC.patched -b "amfi=0xff cs_enforcement_disable=1 -v rd=md0 nand-enable-reformat=1 -progress"
-    ./img4 -i iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
-    ./img4 -i iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
-    ./img4 -i kernelcache.release.n51 -o kernelcache.im4p -k 03447866614ec7f0e083eba37b31f1a75484c5ab65e00e895b95db81b873d1292f766e614c754ec523b62a48d33664e1 -D
-    ./img4 -i kernelcache.im4p -o kernelcache.img4 -M IM4M -T rkrn
-    ./img4 -i DeviceTree.n51ap.im4p -o dtree.raw -k 2f744c5a6cda23c30eccb2fcac9aff2222ad2b37ed96f14a3988102558e0920905536622b1e78288c2533a7de5d01425
-    ./img4 -i dtree.raw -o devicetree.img4 -A -M IM4M -T rdtr
+    ./img4tool -c ramdisk/ramdisk.im4p -t rdsk ramdisk/RestoreRamDisk.dec
+    ./img4tool -c ramdisk/ramdisk.img4 -p ramdisk/ramdisk.im4p -m IM4M
+    ./ipatcher ramdisk/iBSS.dec ramdisk/iBSS.patched
+    ./ipatcher ramdisk/iBEC.dec ramdisk/iBEC.patched -b "amfi=0xff cs_enforcement_disable=1 -v rd=md0 nand-enable-reformat=1 -progress"
+    ./img4 -i ramdisk/iBSS.patched -o iBSS.img4 -M IM4M -A -T ibss
+    ./img4 -i ramdisk/iBEC.patched -o iBEC.img4 -M IM4M -A -T ibec
+    ./img4 -i ramdisk/kernelcache.dec -o ramdisk/kernelcache.img4 -M IM4M -T rkrn
+    ./img4 -i ramdisk/devicetree.dec -o ramdisk/devicetree.img4 -A -M IM4M -T rdtr
 fi
 mkdir work
 cp IM4M work
