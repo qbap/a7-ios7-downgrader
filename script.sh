c54148f53658ddf85965bc79329183d111ff0956
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-02-27 12:49:12 -0600

    ok

--- script.sh
+++ script.sh
@@ -193,9 +193,9 @@ _download_boot_files() {
             # we need to apply mount_common patch for rootfs rw and vm_map_enter patch for tweak injection
             #./Kernel64Patcher $1/$3/kcache.patched $1/$3/kcache2.patched -m -e
             cp $1/$3/kcache.patched $1/$3/kcache2.patched
-            ./kerneldiff jb/12A4297e_kcache.raw $1/$3/kcache2.patched $1/$3/kc.bpatch
-            ./img4 -i jb/12A4297e_kernelcache.dec -o $1/$3/kernelcache.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
-            ./img4 -i jb/12A4297e_kernelcache.dec -o $1/$3/kernelcache -M IM4M -T krnl -P $1/$3/kc.bpatch
+            ./kerneldiff jb/12A4265u_kcache.raw $1/$3/kcache2.patched $1/$3/kc.bpatch
+            ./img4 -i jb/12A4265u_kernelcache.dec -o $1/$3/kernelcache.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
+            ./img4 -i jb/12A4265u_kernelcache.dec -o $1/$3/kernelcache -M IM4M -T krnl -P $1/$3/kc.bpatch
         else
             ./seprmvr64lite $1/$3/kcache.raw $1/$3/kcache.patched
             # we need to apply mount_common patch for rootfs rw and vm_map_enter patch for tweak injection
