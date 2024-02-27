adb885007830c5608b31176bd5553053f150387d
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-02-27 16:19:24 -0600

    idk

--- script.sh
+++ script.sh
@@ -205,6 +205,20 @@ _download_boot_files() {
             ./kerneldiff jb/12A4265u_kcache.raw $1/$3/kcache2.patched $1/$3/kc.bpatch
             ./img4 -i jb/12A4265u_kernelcache.dec -o $1/$3/kernelcache2.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
             ./img4 -i jb/12A4265u_kernelcache.dec -o $1/$3/kernelcache2 -M IM4M -T krnl -P $1/$3/kc.bpatch
+            
+            rm $1/$3/kcache.patched
+            rm $1/$3/kcache2.patched
+            rm $1/$3/kc.bpatch
+            
+            ./img4 -i $1/$3/iBSS.patched -o $1/$3/iBSS.img4 -M IM4M -A -T ibss
+            ./img4 -i $1/$3/iBEC.patched -o $1/$3/iBEC.img4 -M IM4M -A -T ibec
+            ./seprmvr64lite $1/$3/kcache.raw $1/$3/kcache.patched
+            # we need to apply mount_common patch for rootfs rw and vm_map_enter patch for tweak injection
+            #./Kernel64Patcher $1/$3/kcache.patched $1/$3/kcache2.patched -m -e
+            cp $1/$3/kcache.patched $1/$3/kcache2.patched
+            ./kerneldiff $1/$3/kcache.raw $1/$3/kcache2.patched $1/$3/kc.bpatch
+            ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache3.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
+            ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache3 -M IM4M -T krnl -P $1/$3/kc.bpatch
         fi
         ./img4 -i $1/$3/DeviceTree.dec -o $1/$3/devicetree.img4 -A -M IM4M -T rdtr
     fi
@@ -530,11 +544,11 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
         ../../irecovery -f iBEC.img4
         ../../irecovery -f devicetree.img4
         ../../irecovery -c devicetree
-        #if [[ "$1" == *"8"* ]]; then
-        #    ../../irecovery -f kernelcache2.img4
-        #else
+        if [[ "$1" == *"8"* ]]; then
+            ../../irecovery -f kernelcache3.img4
+        else
             ../../irecovery -f kernelcache.img4
-        #fi
+        fi
         ../../irecovery -c bootx &
         cd ../../
     fi
@@ -547,6 +561,29 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
     echo "this should in theory let us enable manual stashing via /.cydia_no_stash"
     echo "which, after unstashing a few things, should fix safari, maps, mail, etc"
     _wait_for_dfu
+    if [[ "$1" == *"8"* ]]; then
+        if [ -e $deviceid/$1/iBSS.img4 ]; then
+            if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); then
+                ./dfuhelper.sh
+            fi
+            _wait_for_dfu
+            cd $deviceid/$1
+            ../../ipwnder -p
+            ../../irecovery -f iBSS.img4
+            ../../irecovery -f iBSS.img4
+            ../../irecovery -f iBEC.img4
+            ../../irecovery -f devicetree.img4
+            ../../irecovery -c devicetree
+            if [[ "$1" == *"8"* ]]; then
+                ../../irecovery -f kernelcache3.img4
+            else
+                ../../irecovery -f kernelcache.img4
+            fi
+            ../../irecovery -c bootx &
+            cd ../../
+        fi
+        _wait_for_dfu
+    fi
     cd ramdisk
     ../ipwnder -p
     ../irecovery -f iBSS.img4
@@ -565,14 +602,16 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/sbin/mount -w -t hfs -o suid,dev /dev/disk0s1s2 /mnt2"
     if [[ "$1" == *"9"* ]]; then
         ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
+    elif [[ "$1" == *"8"* ]]; then
+        ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
     else
         ./sshpass -p "alpine" scp -P 2222 ./jb/fstab root@localhost:/mnt1/etc/
     fi
     if [[ "$1" == *"7"* ]]; then
         ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "mv /mnt1/System/Library/LaunchDaemons/com.apple.CommCenter.plist /mnt1/System/Library/LaunchDaemons/com.apple.CommCenter.plis_"
         ./sshpass -p "alpine" scp -P 2222 ./jb/com.apple.springboard.plist root@localhost:/mnt2/mobile/Library/Preferences/com.apple.springboard.plist
+        ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/System/Library/Caches/com.apple.dyld/enable-dylibs-to-override-cache"
     fi
-    ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/System/Library/Caches/com.apple.dyld/enable-dylibs-to-override-cache"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'rm /mnt1/Applications'
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'mv $(find /mnt2/stash -name Applications) /mnt1/'
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'rm /mnt1/Library/Ringtones'
@@ -617,8 +656,8 @@ else
     ./sshpass -p "alpine" scp -P 2222 ./$deviceid/$1/kernelcache root@localhost:/mnt1/System/Library/Caches/com.apple.kernelcaches
     if [[ "$1" == *"7"* ]]; then
         ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "mv /mnt1/System/Library/LaunchDaemons/com.apple.CommCenter.plist /mnt1/System/Library/LaunchDaemons/com.apple.CommCenter.plis_"
+        ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/System/Library/Caches/com.apple.dyld/enable-dylibs-to-override-cache"
     fi
-    ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/System/Library/Caches/com.apple.dyld/enable-dylibs-to-override-cache"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/.cydia_no_stash"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "chown root:wheel /mnt1/.cydia_no_stash"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "chmod 777 /mnt1/.cydia_no_stash"
@@ -635,6 +674,8 @@ else
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'rm /mnt1/usr/share'
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'mv $(find /mnt2/stash -name share) /mnt1/usr/'
     if [[ "$1" == *"9"* ]]; then
+        ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
+    elif [[ "$1" == *"8"* ]]; then
         ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
     else
         ./sshpass -p "alpine" scp -P 2222 ./jb/fstab root@localhost:/mnt1/etc/
