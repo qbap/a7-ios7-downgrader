462a6c3a693286526b24c7d34fa7e2071e769135
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-09 15:27:28 -0600

    test

--- script.sh
+++ script.sh
@@ -108,10 +108,10 @@ _download_ramdisk_boot_files() {
         # we need to download restore ramdisk for ios 8.4.1
         # in this example we are using a modified copy of the ssh tar from SSHRD_Script https://github.com/verygenericname/SSHRD_Script
         # this modified copy of the ssh tar fixes a few issues on ios 8 and adds some executables we need
-        hdiutil resize -size 210M ramdisk/RestoreRamDisk.dmg
+        hdiutil resize -size 100M ramdisk/RestoreRamDisk.dmg
         hdiutil attach -mountpoint /tmp/ramdisk ramdisk/RestoreRamDisk.dmg
         sudo diskutil enableOwnership /tmp/ramdisk
-        sudo ./gnutar -xzvf ssh.tar.gz -C /tmp/ramdisk
+        sudo ./gnutar -xzvf ssh.tar -C /tmp/ramdisk
         hdiutil detach /tmp/ramdisk
         ./img4 -i ramdisk/RestoreRamDisk.dmg -o ramdisk/ramdisk.img4 -M IM4M -A -T rdsk
         ./iBoot64Patcher ramdisk/iBSS.dec ramdisk/iBSS.patched
