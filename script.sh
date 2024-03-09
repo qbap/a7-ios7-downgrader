f3f0914368057be3cc07c8aa37ea7a99852ae731
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-09 16:14:09 -0600

    test

--- script.sh
+++ script.sh
@@ -379,7 +379,11 @@ elif [[ "$1" == *"9"* ]]; then
     echo "see https://files.catbox.moe/wn83g9.mp4 for a video example"
     exit
 fi
-_download_ramdisk_boot_files $deviceid $replace 11.4
+if [ -z "$2" ]; then
+    echo "./script.sh <target os ver> <current os ver>"
+    exit
+fi
+_download_ramdisk_boot_files $deviceid $replace $2
 _download_boot_files $deviceid $replace $1
 _download_root_fs $deviceid $replace $1
 if [ -e $deviceid/$1/iBSS.img4 ]; then
