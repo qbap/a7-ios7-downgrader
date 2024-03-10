79a84fbd639406616b357b782cb7d5091feb0af8
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-09 18:51:16 -0600

    test

--- script.sh
+++ script.sh
@@ -221,7 +221,8 @@ _download_boot_files() {
                 ./seprmvr64lite $1/$3/kcache.raw $1/$3/kcache.patched
                 # we need to apply mount_common patch for rootfs rw and vm_map_enter patch for tweak injection
                 # app store works perfectly, and so does tweaks
-                ./Kernel64Patcher $1/$3/kcache.patched $1/$3/kcache2.patched -a -m
+                #./Kernel64Patcher $1/$3/kcache.patched $1/$3/kcache2.patched -a -m
+                cp $1/$3/kcache.patched $1/$3/kcache2.patched
                 ./kerneldiff $1/$3/kcache.raw $1/$3/kcache2.patched $1/$3/kc.bpatch
                 ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache.img4 -M IM4M -T rkrn -P $1/$3/kc.bpatch
                 ./img4 -i $1/$3/kernelcache.dec -o $1/$3/kernelcache -M IM4M -T krnl -P $1/$3/kc.bpatch
@@ -580,6 +581,8 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
         # as of right now we have not tested any rootfs rw patches for ios 9
         # we are waiting on a sandbox patch before we can do anything in that regard
         ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
+    elif [[ "$deviceid" == "iPhone7,2" || "$deviceid" == "iPhone7,1" ]]; then
+        ./sshpass -p "alpine" scp -P 2222 ./fstab root@localhost:/mnt1/etc/
     else
         ./sshpass -p "alpine" scp -P 2222 ./jb/fstab root@localhost:/mnt1/etc/
     fi
@@ -674,14 +677,14 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
         if [[ "$deviceid" == "iPhone7,2" || "$deviceid" == "iPhone7,1" ]]; then
             ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'rm -rf /mnt1/System/Library/DataClassMigrators/MobileSafari.migrator/'
             ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'rm -rf /mnt1/System/Library/DataClassMigrators/MapsDataClassMigrator.migrator/'
-            if [[ "$1" == "8.0" ]]; then
-                ./sshpass -p "alpine" scp -P 2222 ./jb/com.apple.datamigrator_patched root@localhost:/mnt1/System/Library/PrivateFrameworks/DataMigration.framework/XPCServices/com.apple.datamigrator.xpc/com.apple.datamigrator
-            elif [[ "$1" == "8.4.1" ]]; then
-                ./sshpass -p "alpine" scp -P 2222 ./jb/com.apple.datamigrator_8.4.1_patched root@localhost:/mnt1/System/Library/PrivateFrameworks/DataMigration.framework/XPCServices/com.apple.datamigrator.xpc/com.apple.datamigrator
-            fi
         fi
+        ./sshpass -p "alpine" scp -P 2222 root@localhost:/mnt1/System/Library/PrivateFrameworks/DataMigration.framework/XPCServices/com.apple.datamigrator.xpc/com.apple.datamigrator ./$deviceid/$1/com.apple.datamigrator
+        ./datamigrator64patcher ./$deviceid/$1/com.apple.datamigrator ./$deviceid/$1/com.apple.datamigrator_patched -n
+        ./sshpass -p "alpine" scp -P 2222 ./$deviceid/$1/com.apple.datamigrator_patched root@localhost:/mnt1/System/Library/PrivateFrameworks/DataMigration.framework/XPCServices/com.apple.datamigrator.xpc/com.apple.datamigrator
         # fix sideloading apps
-        ./sshpass -p "alpine" scp -P 2222 ./jb/lockdownd root@localhost:/mnt1/usr/libexec/lockdownd
+        if [[ "$1" == "8.0" ]]; then
+            ./sshpass -p "alpine" scp -P 2222 ./jb/lockdownd root@localhost:/mnt1/usr/libexec/lockdownd
+        fi
     elif [[ "$1" == *"7"* ]]; then
         ./sshpass -p "alpine" scp -P 2222 ./jb/AppleInternal.tar root@localhost:/mnt1/
         ./sshpass -p "alpine" scp -P 2222 ./jb/PrototypeTools.framework.tar root@localhost:/mnt1/
