199397afe4d9bad908ba7658ab080e98b590ad0e
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-13 10:07:56 -0500

    test

--- script.sh
+++ script.sh
@@ -655,8 +655,8 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
     # fix cydia launch daemon not running at boot
     ./sshpass -p "alpine" scp -P 2222 ./jb/com.saurik.Cydia.Startup.plist root@localhost:/mnt1/System/Library/LaunchDaemons
     # uicache -a on every boot
-    ./sshpass -p "alpine" scp -P 2222 ./jb/startup root@localhost:/mnt1/usr/libexec/cydia/startup
-    ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/usr/sbin/chown root:wheel /mnt1/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist"
+    #./sshpass -p "alpine" scp -P 2222 ./jb/startup root@localhost:/mnt1/usr/libexec/cydia/startup
+    #./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/usr/sbin/chown root:wheel /mnt1/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "rm -rf /mnt2/OS.tar"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "rm -rf /mnt2/log/asl/SweepStore"
     ./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "rm -rf /mnt2/mobile/Library/PreinstalledAssets/*"
