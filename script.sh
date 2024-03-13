a58b9ad75e8c59085f19961badbc4717144ca7ea
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-13 13:32:19 -0500

    test

--- script.sh
+++ script.sh
@@ -4,13 +4,6 @@ oscheck=$(uname)
 version="$1"
 dir="$(pwd)/"
 
-if [ -z "$2" ]; then
-    if [[ ! -e ./$deviceid/0.0/apticket.der || ! -e ./$deviceid/0.0/sep-firmware.img4 || ! -e ./$deviceid/0.0/Baseband || ! -e ./$deviceid/0.0/keybags ]]; then
-        echo "./script.sh <target os ver> <current os ver>"
-        exit
-    fi
-fi
-
 _wait_for_dfu() {
     if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); then
         echo "[*] Waiting for device in DFU mode"
@@ -408,6 +401,12 @@ check=$(./irecovery -q | grep CPID | sed 's/CPID: //')
 replace=$(./irecovery -q | grep MODEL | sed 's/MODEL: //')
 deviceid=$(./irecovery -q | grep PRODUCT | sed 's/PRODUCT: //')
 echo $deviceid
+if [ -z "$2" ]; then
+    if [[ ! -e ./$deviceid/0.0/apticket.der || ! -e ./$deviceid/0.0/sep-firmware.img4 || ! -e ./$deviceid/0.0/Baseband || ! -e ./$deviceid/0.0/keybags ]]; then
+        echo "./script.sh <target os ver> <current os ver>"
+        exit
+    fi
+fi
 # we need a shsh file that we can use in order to boot the ios 8 ramdisk
 # in this case we are going to use the ones from SSHRD_Script https://github.com/verygenericname/SSHRD_Script
 ./img4tool -e -s other/shsh/"${check}".shsh -m IM4M
