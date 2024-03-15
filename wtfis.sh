ac6c71867112ab88604632e4e1687f835b137b79
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-15 13:16:46 -0500

    cleanup

--- wtfis.sh
+++
@@ -1,4 +0,0 @@
-./Darwin/sshpass -p "alpine" scp -P 2222 ../wtfis/Payload/wtfis.app/wtfis root@localhost:/mnt1/Applications/wtfis.app/wtfis
-./Darwin/sshpass -p "alpine" scp -P 2222 ../wtfis/untether/evasi0n7 root@localhost:/mnt1/wtfis/evasi0n7
-./Darwin/sshpass -p "alpine" scp -P 2222 ../wtfis/untether/panguaxe root@localhost:/mnt1/wtfis/panguaxe
-
