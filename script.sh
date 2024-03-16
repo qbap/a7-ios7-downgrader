6c336c269bbb6b019ac5e83a2b24cb86fb4ca9ce
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-16 13:04:15 -0500

    a

--- script.sh
+++ script.sh
@@ -568,8 +568,12 @@ if [[ "$r" = 'yes' || "$r" = 'y' ]]; then
     elif [[ "$1" == "9."*  ]]; then
         "$bin"/sshpass -p "alpine" scp -P 2222 "$dir"/jb/untether_ios9.tar root@localhost:/mnt1/
         "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'tar --preserve-permissions -xvf /mnt1/untether_ios9.tar -C /mnt1/'
-        "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'cp /mnt1/usr/libexec/keybagd /mnt1/usr/libexec/keybagd_o'
-        #"$bin"/sshpass -p "alpine" scp -P 2222 "$dir"/jb/keybagd root@localhost:/mnt1/usr/libexec/keybagd
+        "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'mv /mnt1/usr/libexec/CrashHousekeeping /mnt1/usr/libexec/CrashHousekeeping_o'
+        if [[ "$1" == "9.0" || "$1" == "9.0.1" || "$1" == "9.0.2" ]]; then
+            "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'cp /mnt1/pguntether_90 /mnt1/usr/libexec/CrashHousekeeping'
+        elif [[ "$1" == "9.1" ]]; then
+            "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost 'cp /mnt1/pguntether_91 /mnt1/usr/libexec/CrashHousekeeping'
+        fi
         "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/.pginstalled"
         "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "chmod 777 /mnt1/.pginstalled"
         "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "touch /mnt1/.pg_inst"
