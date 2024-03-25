5a883d2ad3f5dff430485d2b38bf6d93427b9503
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-25 12:07:25 -0500

    test

--- script.sh
+++ script.sh
@@ -492,7 +492,7 @@ if [[ "$ramdisk" == 1 || "$restore" == 1 || "$dump_blobs" == 1 ]]; then
     fi
     "$bin"/irecovery -f kernelcache.img4
     "$bin"/irecovery -c bootx &
-    cd ../../
+    cd ../../../
     sleep 8
     "$bin"/iproxy 2222 22 &
     sleep 2
@@ -584,7 +584,7 @@ if [[ "$ramdisk" == 1 || "$restore" == 1 || "$dump_blobs" == 1 ]]; then
         fi
         "$bin"/irecovery -f kernelcache.img4
         "$bin"/irecovery -c bootx &
-        cd ../../
+        cd ../../../
         sleep 8
         "$bin"/iproxy 2222 22 &
         "$bin"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "cat /gpt.txt | gptfdisk /dev/rdisk0s1" 2> /dev/null
